5dea86748e91ded49f0819bf437d4d9137f599a2
NITESH KUMAR OJHA <125625396+Niteshkumar7389@users.noreply.github.com>
2025-08-14 14:23:47 +0530

    Update auth.controller.js

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -59,8 +59,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, //hr*min*sec*miliseconds
-      sameSite: "strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(201).json(user);
